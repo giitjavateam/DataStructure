a39f41059236c584f0baf6e5f02ed2bb0e5a0eed
ritz-293 <singhreetika.2000@gmail.com>
2021-08-25 10:21:26 +0530

    Array2DSparseMatrixSaddlePointReetika

---
+++ SparseMatrix.java
@@ -0,0 +1,56 @@
+package ds.practice.arrays;
+
+import java.util.Scanner;
+
+//Find the saddle point(minimum element in its row and maximum in its column) in a sparse matrix
+public class SparseMatrix {
+	static boolean findSaddlePoint(int mat[][    ], int n)
+    {
+        
+        for (int i = 0; i < n; i++)
+        {
+            // Find the minimum element of row i.
+            // Also find column index of the minimum element
+            int min_row = mat[i][0], col_ind = 0;
+            for (int j = 1; j < n; j++)
+            {
+                if (min_row > mat[i][j])
+                {
+                    min_row = mat[i][j];
+                    col_ind = j;
+                }
+            }
+      
+            // Check if the minimum element of row is also the maximum element of column or not
+            int k;
+            for (k = 0; k < n; k++)
+      
+                // Note that col_ind is fixed
+                if (min_row < mat[k][col_ind])
+                    break;
+      
+            // If saddle point is present in this row then print it
+            if (k == n)
+            {
+               System.out.println("Value of Saddle Point " + min_row);
+               return true;
+            }
+        }
+      
+        // If Saddle Point not found
+        return false;
+    }
+     
+   
+    public static void main(String[] args)
+    {
+        int mat[][] = {{1, 2, 3},
+                      {4, 5, 6},
+                     {7, 8, 9}};
+         
+        int n = 3;
+        if (findSaddlePoint(mat, n) == false)
+            System.out.println("No Saddle Point ");
+    }
+
+}
